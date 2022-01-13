a1ac9f12511d1838cbfb27d49a31cd5b682fa226
Eduardo Sanchez <eddiekidd98@gmail.com>
2022-01-12 19:02:10 -0600

    code already working

---
+++ tic-tac-toe-EddSJ.py
@@ -0,0 +1,70 @@
+def game(numbers):
+    game_state = 'on'
+    moves = 0
+    while game_state != 'done':
+        
+        print(moves)
+        board(numbers)
+        player_one = int(input('choose your cell: '))
+        player_one_index = player_one - 1
+        if player_one:
+            numbers[player_one_index] = 'x'
+            moves += 1
+            if moves == 9:
+                print('draw')
+                break
+            if has_winner(numbers):
+                game_state = 'done'
+                board(numbers)
+                print('winner')
+                break
+
+        board(numbers)
+        player_two = int(input('choose your cell: '))
+        player_two_index = player_two - 1
+        if player_two:
+            numbers[player_two_index] = 'o'
+            moves += 1
+            if has_winner(numbers):
+                game_state = 'done'
+                board(numbers)
+                print('winner')
+                break
+
+
+
+    
+
+
+def board(numbers):
+    print(f"""
+___________
+   |   |
+ {numbers[0]} | {numbers[1]} | {numbers[2]} 
+___|___|___
+   |   |
+ {numbers[3]} | {numbers[4]} | {numbers[5]}
+___|___|___
+   |   |  
+ {numbers[6]} | {numbers[7]} | {numbers[8]} 
+___|___|___    
+""")
+
+def has_winner(numbers):
+    return (numbers[0] == numbers[1] == numbers[2] or
+            numbers[3] == numbers[4] == numbers[5] or
+            numbers[6] == numbers[7] == numbers[8] or
+            numbers[0] == numbers[3] == numbers[6] or
+            numbers[1] == numbers[4] == numbers[7] or
+            numbers[2] == numbers[5] == numbers[8] or
+            numbers[0] == numbers[4] == numbers[8] or
+            numbers[2] == numbers[4] == numbers[6]) 
+
+
+
+def main():
+    numbers = ['1','2','3','4','5','6','7','8','9']
+    game(numbers)
+
+if __name__ == '__main__':
+    main()
