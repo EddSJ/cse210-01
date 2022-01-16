17bb4da8fa8e6a77cbd9d08c1f05a676570e5819
Eduardo Sanchez <eddiekidd98@gmail.com>
2022-01-15 22:36:18 -0600

    second commit still working

--- tic-tac-toe-EddSJ.py
+++ tic-tac-toe-EddSJ.py
@@ -7,29 +7,36 @@ def game(numbers):
         board(numbers)
         player_one = int(input('choose your cell: '))
         player_one_index = player_one - 1
-        if player_one:
-            numbers[player_one_index] = 'x'
-            moves += 1
-            if moves == 9:
-                print('draw')
-                break
-            if has_winner(numbers):
-                game_state = 'done'
-                board(numbers)
-                print('winner')
-                break
 
-        board(numbers)
-        player_two = int(input('choose your cell: '))
-        player_two_index = player_two - 1
-        if player_two:
-            numbers[player_two_index] = 'o'
-            moves += 1
-            if has_winner(numbers):
-                game_state = 'done'
-                board(numbers)
-                print('winner')
-                break
+        if numbers[player_one_index] != 'x' and numbers[player_one_index] != 'o':
+        
+            if player_one:
+                numbers[player_one_index] = 'x'
+                moves += 1
+                if moves == 9:
+                    print('draw')
+                    break
+                if winner(numbers):
+                    game_state = 'done'
+                    board(numbers)
+                    print('winner')
+                    break
+
+            board(numbers)
+            player_two = int(input('choose your cell: '))
+            player_two_index = player_two - 1
+
+            if player_two:
+                numbers[player_two_index] = 'o'
+                moves += 1
+                if winner(numbers):
+                    game_state = 'done'
+                    board(numbers)
+                    print('winner')
+                    break
+        
+        print('cell is taken')
+
 
 
 
@@ -50,7 +57,7 @@ ___|___|___
 ___|___|___    
 """)
 
-def has_winner(numbers):
+def winner(numbers):
     return (numbers[0] == numbers[1] == numbers[2] or
             numbers[3] == numbers[4] == numbers[5] or
             numbers[6] == numbers[7] == numbers[8] or
